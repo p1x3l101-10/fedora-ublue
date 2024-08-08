d0a95b220e93200e1e2c4b4315d6dbe991f178d6
Pixel <scott.blatt.0b10@gmail.com>
2024-08-08 15:41:20 -0500

    remove solaar

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ wget --output-document='/etc/yum.repos.d/home:mkittler.repo' 'https://download.o
 sed -i '0,/skip_if_unavailable/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
 
 rpm-ostree install steam-devices syncthing syncthingctl-qt6 syncthingfileitemaction-qt6 syncthingplasmoid-qt6
-rpm-ostree uninstall filelight kwalletmanager5
+rpm-ostree uninstall filelight kwalletmanager5 solaar
