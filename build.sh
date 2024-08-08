63e5b6e1d538abcedb692e54cfd3d4bb6ef78f94
Pixel <scott.blatt.0b10@gmail.com>
2024-08-08 15:24:48 -0500

    Initial config

--- build.sh
+++ build.sh
@@ -4,20 +4,8 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+wget --output-document='/etc/yum.repos.d/home:mkittler.repo' 'https://download.opensuse.org/repositories/home:/mkittler/Fedora_40/home:mkittler.repo'
+sed -i '0,/skip_if_unavailable/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+rpm-ostree install steam-devices syncthing syncthingctl-qt6 syncthingfileitemaction-qt6 syncthingplasmoid-qt6
+rpm-ostree uninstall filelight kwalletmanager5
